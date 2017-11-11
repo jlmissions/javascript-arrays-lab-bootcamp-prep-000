a663d19eaeda01afce3d8f53713ae2c87d6da905
jlmissions@yahoo.com <jlmissions@yahoo.com>
2017-11-11 04:17:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,29 @@ function destructivelyAppendKitten (name){
   kittens.push (name)
   return kittens
 }
+function destructivelyPrependKitten (name){
+  kittens.unshift (name)
+  return kittens
+}
+function destructivelyRemoveLastKitten (){
+  kittens.pop ()
+  return kittens
+}
+function destructivelyRemoveFirstKitten (){
+  kittens.shift ()
+  return kittens
+}
+function appendKitten (name){
+  return [...kittens, name]
+}
+function prependKitten (name){
+  return [name, ...kittens]
+}
+function removeLastKitten (){
+  kittens.slice (0, kittens.length - 1)
+  return kittens 
+}
+function removeFirstKitten (){
+  kittens.slice (1)
+  return kittens
+}
