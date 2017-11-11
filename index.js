87a6ef5f922303613ff14c14ea88f176b5683e16
jlmissions@yahoo.com <jlmissions@yahoo.com>
2017-11-11 04:17:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function prependKitten (name){
 }
 function removeLastKitten (){
   kittens.slice (0, kittens.length - 1)
-  return kittens 
+  return kittens
 }
 function removeFirstKitten (){
   kittens.slice (1)
